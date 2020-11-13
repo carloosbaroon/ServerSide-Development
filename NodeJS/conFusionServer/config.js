f2b3ee81dabdc7236eea8f19778500c281a4d486
Carlos Baron <carlostectol@gmail.com>
2020-11-13 08:50:45 -0600

    Passport FB

--- NodeJS/conFusionServer/config.js
+++ NodeJS/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId:'377948609939130',
+        clientSecret: '588e285bf25e6f4e664c964565994294'
+    }
 }
